1b2b68f95febd25f905c021799879c613dfc2035
unknown <>
2016-12-09 12:47:33 +0900

    変更テスト

--- CoRe/src/Test.java
+++ CoRe/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String args[]) {
 		try {
 			database.MySqlQuery sql = new database.MySqlQuery();
-			System.out.println("接続に成功");
+			System.out.println("接続に成功しました");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
